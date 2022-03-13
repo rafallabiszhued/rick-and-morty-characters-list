eaf52b02c1ebe12da7af544fde9402e743391403
rafallabisz <rafal0710@gmail.com>
2022-03-13 22:28:33 +0100

    loader update style

--- src/common/Alert/Alert.tsx
+++ src/common/Alert/Alert.tsx
@@ -6,7 +6,7 @@ interface AlertProps {
 
 const Alert: React.FC<AlertProps> = ({ children }) => {
   return (
-    <div className="bg-green-100 rounded-lg py-5 px-6 mb-4 text-base text-green-700 mb-3" role="alert">
+    <div className="bg-green-100 rounded-lg py-5 px-6 mb-4 text-base text-green-700 mb-3 text-center" role="alert">
       {children}
     </div>
   );
--- src/common/LoadingIndicator/LoadingIndicator.tsx
+++ src/common/LoadingIndicator/LoadingIndicator.tsx
@@ -1,11 +1,24 @@
 import React from 'react';
+import LoadingLogo from 'assets/loading.svg';
+import styled from 'styled-components/macro';
+
+const LoadingContainer = styled.div`
+  position: relative;
+  min-height: 100px;
+  img {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
 
 interface LoadingIndicatorProps {
   isLoading: boolean;
 }
 
 const LoadingIndicator: React.FC<LoadingIndicatorProps> = ({ isLoading }) => {
-  return <>{isLoading && <div>Loading...</div>}</>;
+  return <LoadingContainer>{isLoading && <img src={LoadingLogo} alt={'Loading...'} />}</LoadingContainer>;
 };
 
 export default LoadingIndicator;
